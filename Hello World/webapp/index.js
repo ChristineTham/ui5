3757a59c7730bd47aeb7a480a6698751e00e59e2
Chris Tham <chris.tham@hellotham.com>
2019-05-12 14:57:42 +1000

    Step 2 Steady

--- Hello World/webapp/index.js
+++ Hello World/webapp/index.js
@@ -4,11 +4,12 @@ sap.ui.define([
 ], function (Button, MessageToast) {
 	"use strict";
 
-	new Button({
-		text: "Ready...",
-		press: function () {
-			MessageToast.show("Hello World!");
-		}
-	}).placeAt("content");
+	sap.ui.require([
+		"sap/ui/core/mvc/XMLView"
+	], function (XMLView) {
+		XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
+			oView.placeAt("content");
+		});
+	});
 
 });
